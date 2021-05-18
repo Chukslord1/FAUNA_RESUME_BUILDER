0639d5a8a50ffc0321f262842d4ebfc57eb171aa
chukslord1 <chukslord1@gmail.com>
2021-05-18 01:28:12 +0100

    checked for corrections

--- App/views.py
+++ App/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
-from django.core.paginator import Paginator
 from django.http import HttpResponseNotFound
 from faunadb import query as q
 import pytz
@@ -11,7 +10,7 @@ import datetime
 
 
 
-client = FaunaClient(secret="SECRET_KEY")
+client = FaunaClient(secret="FAUNA_SECRET_KEY")
 indexes = client.query(q.paginate(q.indexes()))
 
 # Create your views here.
